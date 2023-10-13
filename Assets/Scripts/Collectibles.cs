89c7b96304bb2cf4afead5b9503a6fb93c8379c7
Luke Hirst <521480@chrome.escg.ac.uk>
2023-10-13 11:14:52 +0100

    Finishing up
    This is a simple app to show how touching can work.

--- Assets/Scripts/Collectibles.cs
+++ Assets/Scripts/Collectibles.cs
@@ -6,8 +6,10 @@ public class Collectibles : MonoBehaviour
 {
     public void OnCollisionEnter2D(Collision2D collision)
     {
+        // Is the player colliding with the collectible?
         if(collision.gameObject.tag == "Player")
         {
+            // Destroy the collectible.
             Destroy(gameObject);
         }
     }
